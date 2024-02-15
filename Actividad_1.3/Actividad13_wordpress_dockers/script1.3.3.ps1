6c45a799a0e42c2554f84c8fdea424019222a2ed
Juliansr1233 <julian.sainzrodriguez@iesmiguelherrero.com>
2024-02-15 13:30:59 +0100

    Modifico

--- Actividad_1.3/Actividad13_wordpress_dockers/script1.3.3.ps1
+++ Actividad_1.3/Actividad13_wordpress_dockers/script1.3.3.ps1
@@ -25,7 +25,7 @@ apt-get update
 echo "-----------------------------------"
 echo "INSTALACIÓN DEL MOTOR DE DOCKER"
 echo "-----------------------------------"
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 # Paso 4:
 echo "-----------------------------------"
 echo "COMPROBACIÓN INSTALACIÓN DOCKER"
@@ -47,4 +47,60 @@ su ubuntu
 echo "ejecuta:"
 docker run hello-world
 
+# Paso 6: 
+echo "-----------------------------------"
+echo " Creamos carpeta Wordpress"
+echo "-----------------------------------"
+mkdir wordpress
+cd wordpress
+echo "-----------------------------------"
+echo " Creamos la base de datos:"
+echo "-----------------------------------"
 
+echo "MARIADB_DATABASE=wp_database200
+MARIADB_USER=wp_user200
+MARIADB_PASSWORD=alumno200
+MARIADB_ROOT_PASSWORD=alumno200" >> .env
+echo "version: '3.8'
+services:
+  wordpress:
+    image: wordpress
+    ports:
+      - 80:80
+    environment:
+      - WORDPRESS_DB_HOST=mariadb
+      - WORDPRESS_DB_NAME=${MARIADB_DATABASE}
+      - WORDPRESS_DB_USER=${MARIADB_USER}
+      - WORDPRESS_DB_PASSWORD=${MARIADB_PASSWORD}
+    volumes:
+      - wordpress_data:/var/www/html
+    depends_on:
+      - mariadb
+    restart: always
+  mariadb:
+    image: mariadb
+    ports:
+      - 3306:3306
+    environment:
+      - MARIADB_ROOT_PASSWORD=${MARIADB_ROOT_PASSWORD}
+      - MARIADB_DATABASE=${MARIADB_DATABASE}
+      - MARIADB_USER=${MARIADB_USER}
+      - MARIADB_PASSWORD=${MARIADB_PASSWORD}
+    volumes:
+      - mariadb_data:/var/lib/mysql
+    restart: always
+  phpmyadmin:
+    image: phpmyadmin
+    ports:
+      - 8080:80
+    environment:
+      - PMA_ARBITRARY=1
+    restart: always
+volumes:
+  mariadb_data:
+  wordpress_data:" >> docker-compose.yml
+
+echo "-----------------------------------"
+echo " Ejecutamos el docker compose:"
+echo "-----------------------------------"
+docker compose up -d 
